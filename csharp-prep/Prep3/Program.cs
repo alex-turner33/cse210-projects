1855590d1a78c578fc51cd33d0f6633796a02f76
Alex Turner <tur20025@byui.edu>
2025-04-29 12:58:19 -0600

    Created a program that makes you guess a number

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.Data;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string play_again = "";
+        do{
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1,101);
+
+            int guess = -1;
+            int guess_count = 0;
+
+            while (guess != magicNumber){
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                guess_count += 1;
+
+                if (guess > magicNumber){
+                    Console.WriteLine("Lower");
+                } else if (guess < magicNumber){
+                    Console.WriteLine("Higher");
+                } else {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"It took you {guess_count} attempts to guess the magic number {magicNumber}");
+                }
+            }
+
+            Console.Write("Do you want to play again? (yes/no) ");
+            play_again = Console.ReadLine();
+
+        }while(play_again == "yes");
     }
 }
