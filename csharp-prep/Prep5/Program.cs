6255185882143b833fcd4101f55f23efe2821d88
Alex Turner <tur20025@byui.edu>
2025-05-01 13:24:57 -0600

    Created a program that asks the user for their name and a number then displays them using functions

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        int numberSquared = SquareNumber(number);
+        DisplayResult(name, numberSquared);
+    }
+
+   static void DisplayWelcome(){
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName(){
+        Console.Write("Please enter your name: ");
+        
+        return Console.ReadLine();
+    }
+
+    static int PromptUserNumber(){
+        Console.Write("Please enter your favorite number: ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    static int SquareNumber(int number_to_square){
+        return number_to_square * number_to_square;
+    }
+
+    static void DisplayResult(string name, int number){
+        Console.WriteLine($"{name}, the square of your number is {number}");
     }
 }
