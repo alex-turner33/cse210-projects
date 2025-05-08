6c20191d1dfd08ecf9d8e6987a7dd71f1321e360
Alex Turner <tur20025@byui.edu>
2025-05-08 14:58:05 -0600

    Created 3 files. One to handle a resume, one to create jobs and another one to handle the other two

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Resume resume = new Resume();
+        resume._name = "Alex Turner";
+
+        Job job_1 = new Job();
+        job_1._company = "Microsoft";
+        job_1._jobTitle = "Programmer";
+        job_1._startYear = 2021;
+        job_1._endYear = 2024;
+        
+        resume._jobs.Add(job_1);
+
+        Job job_2 = new Job();
+        job_2._company = "Manager";
+        job_2._jobTitle = "T-Mobile";
+        job_2._startYear = 2024;
+        job_2._endYear = 2027;
+
+        resume._jobs.Add(job_2);
+
+        resume.Display();
     }
 }
