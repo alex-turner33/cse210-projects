e2edccbf8a48ccb8a2dee35e4328dd7a5c78172a
alex-turner33 <tur20025@buyi.edu>
2025-06-05 20:27:04 -0600

    AI message near top of Program.cs file. Created a program that will gather information about a class and some of the assignments a student might have in it and to display the correct files. The system used inheritance.

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -2,8 +2,23 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning04 World!");
+    static void Main(string[] args) //Used Chatgpt to solve error with .not for some reason Learning04.csproj had the net as 7.0 
+    { //when it was supposed to be 8.0 like all the others otherwise no AI was used in the creation of this project
+
+        Assignment assignment1 = new Assignment("Jacob Everton", "Math");
+        Console.WriteLine(assignment1.GetSummary());
+
+        Console.WriteLine();
+
+        MathAssignment assignment2 = new MathAssignment("Jared Jackson", "Trig", "4.3", "18-22, 24");
+        Console.WriteLine(assignment2.GetSummary());
+        Console.WriteLine(assignment2.GetHomeworkList());
+
+        Console.WriteLine();
+
+        WritingAssignment assignment3 = new WritingAssignment("Katlynn Mackson", "Writing", "My infamous story");
+        Console.WriteLine(assignment3.GetSummary());
+        Console.WriteLine(assignment3.GetWritingInformation());
+
     }
 }
